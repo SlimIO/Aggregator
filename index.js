14d2296a6d6216d068aba5f06004e267f44bd5a1
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-05-31 11:49:14 +0200

    chore: add uncommited work

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // Require Third-Party Dependencies
 const Addon = require("@slimio/addon");
 
+// Globals
+const Cards = new Map();
+
 // Create Addon
 const Aggregator = new Addon("aggregator").lockOn("events");
 
 Aggregator.on("awake", () => {
     Aggregator.ready();
 });
+
+Aggregator.of("Metric.create").subscribe(async([dbName, id]) => {
+    if (Cards.has(id)) {
+        return;
+    }
+
+    // console.log(`METRIC CREATED - ${dbName}.db, id: ${id}`);
+    try {
+        const mic = await Aggregator.sendOne("events.get_mic", [id]);
+        console.log(mic);
+        Cards.set(id, { dt: Date.now(), mic });
+    }
+    catch (err) {
+        console.error(err);
+    }
+}, console.error);
 
 // Export "Aggregator" addon for Core
 module.exports = Aggregator;
